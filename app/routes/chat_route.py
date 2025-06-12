a80ca3f9c75e0e482d43b5222779b1b2c9c74b6d
Ahsen <ak1998@live.nl>
2025-06-12 22:00:24 +0200

    Gebruiker kan nu een chatbot + chat maken

--- app/routes/chat_route.py
+++ app/routes/chat_route.py
@@ -4,9 +4,8 @@ from flask import request, Blueprint, jsonify
 def create_chat_blueprint(chat_service):
     chat_bp = Blueprint('chat', __name__)
 
-    @chat_bp.route("/create-chat", methods=["POST"])
+    @chat_bp.route("/chat", methods=["POST"])
     def create_chat():
         chat_data = request.get_json()
-        chat_service.create_chat(chat_data)
-        return jsonify({"chat_id": 1}), 201
+        return jsonify(chat_service.create_chat(chat_data))
     return chat_bp
